a172473c7a893f81055e7b23f819cbff8d050026
Artur <49900277+Maserezio@users.noreply.github.com>
2021-11-22 21:50:38 +0300

    almost finished(add button to build graph)

--- app/src/main/java/com/example/calculator/GraphInput.kt
+++ app/src/main/java/com/example/calculator/GraphInput.kt
@@ -1,12 +1,21 @@
 package com.example.calculator
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.FrameLayout
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.graph_input.*
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class GraphInput:BottomSheetDialogFragment() {
+import androidx.annotation.NonNull
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+
+class GraphInput : BottomSheetDialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,7 +24,15 @@ class GraphInput:BottomSheetDialogFragment() {
         return inflater.inflate(R.layout.graph_input, container, false)
     }
 
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-         super.onViewCreated(view, savedInstanceState)
+        super.onViewCreated(view, savedInstanceState)
+            close.setOnClickListener { dismiss() }
+            add.setOnClickListener {
+                val kek:EditText = EditText(this.context)
+                kek.setText("y=")
+                kekek.addView(kek)
+            }
     }
 }
--- app/src/main/java/com/example/calculator/GraphingCalculatorFragment.kt
+++ app/src/main/java/com/example/calculator/GraphingCalculatorFragment.kt
@@ -1,19 +1,18 @@
 package com.example.calculator
 
-import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.example.calculator.databinding.GraphingCalculatorBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.jjoe64.graphview.series.*
 import com.jjoe64.graphview.GraphView
 import kotlinx.android.synthetic.main.graphing_calculator.*
+import net.objecthunter.exp4j.ExpressionBuilder
+
+
+
 
 
 class GraphingCalculatorFragment : Fragment() {
@@ -33,40 +32,38 @@ class GraphingCalculatorFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val add = GraphInput()
+        val graph:GraphView = binding.graph
+        graph.getViewport().setScalable(true)
 
-        button3.setOnClickListener {
-            add.show(childFragmentManager,"bebra")
-        }
 
-//        val graph:GraphView = binding.graph
-//        graph.getViewport().setScalable(true)
-//
-//        val Data = intArrayOf(
-//            10, 46, 53, 58, 63, 67, 69, 72, 75, 78, 82, 85,
-//            90, 95, 99, 105, 110, 115, 121, 126, 132, 137, 143,
-//            148, 153, 157, 162, 165, 168, 170, 173, 174, 175, 176,
-//            177, 177, 177, 176, 176, 175, 173, 172, 171, 169, 168,
-//            167, 166, 164, 161, 155, 147, 136, 123, 111, 101, 92, 84,
-//            78, 74, 70, 67, 65, 64, 62, 61, 61, 60, 60, 59, 59, 58, 58,
-//            58, 57, 57, 57, 56, 56, 56, 56, 56, 56, 56, 55, 55, 55, 55,
-//            55, 54, 54
-//        )
-//
+
 //        val series: LineGraphSeries<DataPoint> = LineGraphSeries<DataPoint>()
 //
-//        var x : Double = 0.0;  var y : Double = 0.0
+//        var x : Double = 0.0;
+//        var y : Double = 0.0
 //        x = -50.0
 //
-//        for (i in -100..500)
+//        for (i in 0..500)
 //        {
 //            x += 0.1
-//            y = Math.sin(x)
+//            y = (input.text).toString().toDouble()
 //            series.appendData(DataPoint(x,y), true, 5000)
 //        }
+//
+//        graph.addSeries(series)
 
-        //graph.addSeries(series)
 
+
+        val series = LineGraphSeries(
+            arrayOf<DataPoint>(
+                DataPoint(0.0, ExpressionBuilder("sin(x)").variable("x").build().setVariable("x", 0.0).evaluate().toDouble()),
+                DataPoint(1.0, ExpressionBuilder("sin(x)").variable("x").build().setVariable("x", 1.0).evaluate().toDouble()),
+                DataPoint(2.0, ExpressionBuilder("sin(x)").variable("x").build().setVariable("x", 2.0).evaluate().toDouble()),
+                DataPoint(3.0, ExpressionBuilder("sin(x)").variable("x").build().setVariable("x", 3.0).evaluate().toDouble()),
+                DataPoint(4.0, ExpressionBuilder("sin(x)").variable("x").build().setVariable("x", 4.0).evaluate().toDouble() )
+            )
+        )
+        graph.addSeries(series)
 
     }
 
